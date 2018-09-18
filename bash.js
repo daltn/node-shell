6be98ee21706a6b1e0b1cb72f8f6e5dd8f112a3c
Dalton Saffe <daltonsaffe@gmail.com>
2018-09-18 12:53:06 -0400

    mmmhmmm

--- bash.js
+++ bash.js
@@ -2,10 +2,10 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  const currentDir = process.cwd();
 
-  process.stdout.write('You typed: ' + cmd + '/////////');
-  process.stdout.write('You typed: ' + currentDir);
+  const pwd = require('./pwd');
 
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write(pwd());
   process.stdout.write('\nprompt > ');
 });
